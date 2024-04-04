71295626fd4a22caa44c1dde3c1a90dbe1b8f2ba
Ewanretor Giwa-Okugbe <ewanretorokugbe@gmail.com>
2024-04-04 22:59:43 +0100

    added coming soon with simple glitch effect

--- app.vue
+++ app.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="h-screen">
+    <head>
+      <title>ZeroFi Labs - Unlocking real-world value for DeFi</title>
+    </head>
+    <div class="glitch-wrapper">
+      <h2 class="hero glitch layers glitch-title" data-text="Coming Soon..."><span>Coming Soon...</span></h2>
+    </div>
   </div>
 </template>
+
+<style>
+@import url("~/assets/css/glitch.css");
+</style>
