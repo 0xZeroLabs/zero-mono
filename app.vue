4bfa914a06eda237cacc6a0f3757b98b9cd2419e
Ewanretor Giwa-Okugbe <ewanretorokugbe@gmail.com>
2024-04-09 10:13:36 +0100

    update

--- app.vue
+++ app.vue
@@ -4,7 +4,18 @@
       <title>ZeroFi Labs - Unlocking real-world value for DeFi</title>
     </head>
     <div class="wrapper">
-      <h2 class="text-8xl" data-text="Coming Soon..."><span>Coming Soon...</span></h2>
+      <div class="w-[88.89%] mb-12 md:mb-16">
+        <h1
+          class="text-2xl mb-2 lg:mb-9 md:text-[clamp(2rem,4vw,4.2rem)] leading-[1.1] capitalize"
+        >
+          Unleashing DeFi's Potential
+        </h1>
+        <h2
+          class="capitalize text-3xl md:text-[clamp(2.7rem,4vw,4rem)] leading-[1.1] font-extrabold"
+        >
+          Empowering a New Digital Age
+        </h2>
+      </div>
     </div>
   </div>
 </template>
