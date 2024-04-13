c453f9cbbf3cd519ffc53fa7c910c7bd697e3b91
Ewanretor Giwa-Okugbe <ewanretorokugbe@gmail.com>
2024-04-13 02:14:05 +0100

    setup monorepo

---
+++ apps/passport/tailwind.config.ts
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+  ],
+  theme: {
+    fontFamily: {
+      sans: ["Montserrat"],
+      serif: ["Montserrat"],
+      mono: ["Montserrat"],
+      display: ["Montserrat"],
+      body: ["Montserrat"],
+    },
+    duration: {
+      fast: "1s",
+      normal: "3s",
+      slow: "9s",
+    },
+  },
+  plugins: [require("@designbycode/tailwindcss-text-glitch"),],
+  extend: {}
+}
--- ddv/nuxt.config.ts
+++
@@ -1,8 +0,0 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  modules: [
-    '@nuxtjs/tailwindcss'
-  ],
-  css: ['~/assets/scss/main.scss'],
-  devtools: { enabled: true }
-})
