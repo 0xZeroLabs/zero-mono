52c421e88fcedec1a4071ba27689e6d64a348de5
Ewanretor Giwa-Okugbe <ewanretorokugbe@gmail.com>
2024-04-20 21:11:07 +0100

    added pwa to passport

--- apps/passport/nuxt.config.ts
+++ apps/passport/nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ["@nuxtjs/tailwindcss", "@vite-pwa/nuxt"],
   css: ["~/assets/scss/main.scss"],
   devtools: { enabled: true },
   devServer: {
