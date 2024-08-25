a571c1aa7585caced59e96d520d3f9ad53be90e9
Ewanretor Giwa-Okugbe <ewanretorokugbe@gmail.com>
2024-08-25 05:39:47 +0100

    made new rounds of update to partners to optimise raise

--- apps/landing/components/Items.vue
+++ apps/landing/components/Items.vue
@@ -2,14 +2,14 @@
     <div class="min-w-[1056px] md:min-w-[1664px] h-fit partner-slide inline-block whitespace-nowrap">
         <div class="flex items-center justify-around">
             <img class="w-44 h-fit md:w-52 logo" src="@/assets/viction-logo.svg" alt="viction" :draggable="false" />
-            <img class="w-44 h-fit md:w-52 logo" src="@/assets/aligned-logo-white.svg" alt="hyperlane"
+            <img class="w-44 h-fit md:w-52 logo" src="@/assets/aligned-logo-white.svg" alt="aligned"
                 :draggable="false" />
-            <img class="w-44 h-fit md:w-52 logo" src="@/assets/phronai-logo.svg" alt="phronai" :draggable="false" />
-            <img class="w-32 h-fit md:w-40 logo" src="@/assets/irys-white.svg" alt="mina" :draggable="false" />
-            <img class="w-44 h-fit md:w-52 logo" src="@/assets/sendtokens-logo.svg" alt="sendtokens"
+            <img class="w-44 h-fit md:w-52 logo hidden" src="@/assets/phronai-logo.svg" alt="phronai" :draggable="false" />
+            <img class="w-32 h-fit md:w-40 logo" src="@/assets/irys-white.svg" alt="irys" :draggable="false" />
+            <img class="w-44 h-fit md:w-52 logo hidden" src="@/assets/sendtokens-logo.svg" alt="sendtokens"
                 :draggable="false" />
-            <img class="w-44 h-fit md:w-52 logo hidden" src="@/assets/0xpass-white.svg" alt="sendtokens" :draggable="false" />
-            <img class="w-44 h-fit md:w-52 logo" src="@/assets/sindri-white.svg" alt="nextbridge" :draggable="false" />
+            <img class="w-44 h-fit md:w-52 logo hidden" src="@/assets/0xpass-white.svg" alt="0xpass" :draggable="false" />
+            <img class="w-44 h-fit md:w-52 logo" src="@/assets/sindri-white.svg" alt="sindri" :draggable="false" />
         </div>
     </div>
 </template>
