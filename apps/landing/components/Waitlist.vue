dff387eb8c26275cecf1b654037a8f885ad03f64
Ewanretor Giwa-Okugbe <ewanretorokugbe@gmail.com>
2024-09-27 09:44:16 +0100

    updated waitlist to match current styling

--- apps/landing/components/Waitlist.vue
+++ apps/landing/components/Waitlist.vue
@@ -7,11 +7,11 @@
             </h2>
             <form method="post" class="w-full rounded-2xl">
                 <input v-model="email" type="email" name="email" placeholder="Email"
-                    class="w-full px-3 outline-1 h-[40px] md:h-[48px] border-[0.5px] border-[#fff]/40 text-white mt-5" />
+                    class="w-full px-3 outline-1 h-[40px] border-[0.5px] border-[#fff]/40 text-white mt-5" />
                 <button type="submit"
-                    class="w-full h-[40px] md:h-[48px] border-[0.5px] border-[#fff] text-white mt-6 btn"
+                    class="w-full h-[40px] border-[0.5px] border-[#fff] text-white mt-6 btn"
                     @click.prevent="submitEmail" @keyup.enter="submitEmail">
-                    <span>{{ value }}</span>
+                    <span><Scramble :content="value" /></span>
                 </button>
                 <WaitlistFeedback :formFeedback="(formFeedback as string)" :isLoading="isLoading" />
             </form>
