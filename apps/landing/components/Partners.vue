24e5ea1f4305c88f5fcf62a32ae28e1aef10cb21
Ewanretor Giwa-Okugbe <ewanretorokugbe@gmail.com>
2024-09-28 20:46:20 +0100

    updated partners carousel sizing

--- apps/landing/components/Partners.vue
+++ apps/landing/components/Partners.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-[#000] w-full h-40 md:h-44 pt-5 flex flex-wrap justify-start items-center border-[#111111] border-y">
+  <div class="bg-[#000] w-full h-32 pt-5 flex flex-wrap justify-start items-center border-[#111111] border-y">
     <div class="h-fit partners overflow-hidden">
         <Items />
         <Items />
