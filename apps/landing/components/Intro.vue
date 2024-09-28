e8227b863c943652b0d47c3fa69c599db9156905
Ewanretor Giwa-Okugbe <ewanretorokugbe@gmail.com>
2024-09-28 20:46:45 +0100

    returned intro section to a unibody design

--- apps/landing/components/Intro.vue
+++ apps/landing/components/Intro.vue
@@ -1,21 +1,16 @@
 <template>
-    <div class="us gap-2 text-sm">
-        <div class="bg-[#e8e8e8] rounded-3xl p-5 text-black w-full column-ends2">
+    <div class="text-sm">
+        <div class="bg-[#e8e8e8] min-h-[260px]lg:min-h-[320px] rounded-3xl p-5 text-black w-full">
             <h3 class="font-bold negative">
                 Foundry of innovation
             </h3>
             <p class="negative font-normal mt-2 text-left z-10 relative">
                 We're a venture studio committed to solving the intricate challenges that hinder self-sovereignty. Our
                 goal is to simplify the process of empowering individuals with control over their data, thereby
-                safeguarding their privacy. Founded in Nigeria in 2024, we're is dedicated to tackling global challenges
+                safeguarding their privacy. Founded in Nigeria, we're dedicated to tackling global challenges
                 and are collaborating with
                 teams internationally to achieve the collective goal of empowering digital freedom.
             </p>
-        </div>
-        <div class="bg-[#fff] rounded-3xl p-5 text-black w-full column-ends">
-            <h3 class="font-bold negative">
-                Made by innovators
-            </h3>
         </div>
     </div>
 </template>
