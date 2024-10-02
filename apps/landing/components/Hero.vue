2b52f9f8f77f9b5c6524f511d9f72eaecdad0ec7
Ewanretor Giwa-Okugbe <ewanretorokugbe@gmail.com>
2024-10-02 01:43:58 +0100

    made genral fixes, including to the footer, header, body padding, and links

--- apps/landing/components/Hero.vue
+++ apps/landing/components/Hero.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="min-w-full min-h-[calc(100vh-252px)] rounded-3xl p-5 mt-[72px] relative overflow-hidden flex justify-start items-center text-black gap-3 bg-white">
+        class="min-w-full min-h-[calc(100vh-252px)] rounded-3xl p-5 mt-[56px] relative overflow-hidden flex justify-start items-center text-black gap-3 bg-white">
         <div class="">
             <img src="@/assets/statue-head.png" alt="" class="hidden absolute z-0 -scale-x-100 w-[400px] h-fit -bottom-[100px] right-0">
             <h1 class="negative font-normal text-[24px] md:text-[32px] text-left mb-4 z-10 relative">
@@ -11,7 +11,7 @@
             </h2>
             <div class="gap-2 flex justify-start items-center z-10 relative">
                 <ScrambleBtn content="Explore protocol" type="inverse-alt" />
-                <a href="https://docs.0xzero.org/developers/zero-sdk"
+                <a href="https://docsend.com/view/2fbpws5p2vsjnqqj"
                     class="negative text-sm"><Scramble content="Read whitepaper" /></a>
             </div>
         </div>
