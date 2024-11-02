d9a347e7800f8aed89006c6ac04dc2ed0781a889
Ewanretor Giwa-Okugbe <ewanretorokugbe@gmail.com>
2024-11-02 01:06:56 +0100

    reorg deletions

--- apps/protocol/wallet/content.ts
+++
@@ -1,9 +0,0 @@
-import { sendToBackground } from "@plasmohq/messaging"
-
-document.addEventListener("keydown", async (event) => {
-  if (event.key === "|") {
-    await sendToBackground({
-      name: "open-popup"
-    })
-  }
-})
