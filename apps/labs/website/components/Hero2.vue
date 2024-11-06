517fca56c6fcd3043360970529b9be038b6a2722
Ewanretor Giwa-Okugbe <ewanretorokugbe@gmail.com>
2024-11-06 16:19:11 +0100

    added company page

---
+++ apps/labs/website/components/Hero2.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="min-w-full min-h-[calc(100vh-252px)] rounded-3xl p-5 mt-[56px] relative overflow-hidden flex flex-col justify-end text-black gap-3 bg-white">
+  <div class="gap-2 flex justify-start items-center">
+    <ScrambleBtn content="Our solutions" type="inverse-alt" />
+    <Scramble content="Read thesis" to="https://docs.0xzero.org" />
+  </div>
+</div>
+</template>
+
+<script lang="ts" setup>
+</script>
