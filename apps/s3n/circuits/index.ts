636073eb099f2baf3ecd1105a547b28aea420138
Ewanretor Giwa-Okugbe <ewanretorokugbe@gmail.com>
2024-11-09 07:29:59 +0100

    deleted old ciruits

--- apps/s3n/circuits/index.ts
+++ apps/s3n/circuits/index.ts
@@ -1,19 +1,12 @@
-import { ethers } from "ethers";
-import { getBytes, hexlify, toUtf8Bytes } from "ethers";
-import { keccak256 } from "ethers";
-
-// proof_generation.ts
 import sindri from "sindri";
 
 export const generateProof = async (
-  message: string,
-  wallet: ethers.HDNodeWallet | ethers.Wallet
 ) => {
-  const circuitInputs = { private_key: "5837da14afbb1229eae18d07700b0e6ec2b6407384a08ef25fde3d55ea846962", message: "hello world" };
+  const circuitInputs = {  };
 
   try {
     const proof = await sindri.proveCircuit(
-      "verify-ecdsa:latest",
+      "proof:latest",
       JSON.stringify(circuitInputs)
     );
     return proof;
@@ -23,8 +16,5 @@ export const generateProof = async (
   }
 };
 
-// usage.ts
-const message = "Hello World";
-const wallet = ethers.Wallet.createRandom();
 
-generateProof(message, wallet).then(console.log);
+generateProof().then(console.log);
