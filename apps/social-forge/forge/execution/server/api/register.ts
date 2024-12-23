ef830e7afd03ad5ca2c8ae59623193808902d34b
hey-ewan <ewanretorokugbe@gmail.com>
2024-12-23 02:06:27 +0100

    untracked

--- apps/social-forge/forge/execution/server/api/register.ts
+++ apps/social-forge/forge/execution/server/api/register.ts
@@ -1,7 +1,21 @@
 import { registerNFTasIP, verifyProof } from "~/controllers/functions";
+import { client } from "~/controllers/utils";
+import { IpMetadata } from "@story-protocol/core-sdk";
 
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
+
+  // const ipMetadata: IpMetadata = client.ipAsset.generateIpMetadata({
+  //   title: "My IP Asset",
+  //   description: "This is a test IP asset",
+  //   watermarkImg: "https://picsum.photos/200",
+  //   attributes: [
+  //     {
+  //       key: "Rarity",
+  //       value: "Legendary",
+  //     },
+  //   ],
+  // });
 
   const response = await registerNFTasIP();
   return {
