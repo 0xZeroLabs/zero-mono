577a01d3fbfc4a9924bf1a4be2746990c072ca56
hey-ewan <ewanretorokugbe@gmail.com>
2025-01-03 22:34:18 +0100

    fix: updated components for company page

--- apps/labs/website/pages/company.vue
+++ apps/labs/website/pages/company.vue
@@ -1,11 +1,9 @@
 <template>
   <div class="min-h-full w-full">
-    <Hero2 />
-    <h3 class="text-white font-normal text-[19px] md:text-[24px] text-center my-2 z-10 relative">
-      About us
-    </h3>
-    <Intro />
-    <h3 class="text-white font-normal text-[19px] md:text-[24px] text-center my-2 z-10 relative">
+    <Intro2 />
+    <h3
+      class="text-white font-normal text-[19px] md:text-[24px] text-center my-2 z-10 relative"
+    >
       Meet the team
     </h3>
     <Team />
@@ -13,5 +11,5 @@
 </template>
 
 <script setup lang="ts">
-useHead({ title: "ZERO | Enabling self-soverignty" })
+useHead({ title: "ZERO | Enabling self-soverignty" });
 </script>
