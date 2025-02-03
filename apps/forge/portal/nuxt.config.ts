2c6659fa27a57e3398f9aa0b426434b92583b36a
hey-ewan <ewanretorokugbe@gmail.com>
2025-02-03 18:29:36 +0100

    fix: pointed to the css file

--- apps/forge/portal/nuxt.config.ts
+++ apps/forge/portal/nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt", "@nuxtjs/color-mode"],
+  css: ["~/assets/scss/main.scss"],
   shadcn: {
     /**
      * Prefix for all the imported component
