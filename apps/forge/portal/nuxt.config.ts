fe98e452d4643ae8b50a50b8736acc0caaeb9810
hey-ewan <ewanretorokugbe@gmail.com>
2025-02-03 18:29:20 +0100

    chore: added `@nuxtjs-colormode`

--- apps/forge/portal/nuxt.config.ts
+++ apps/forge/portal/nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt"],
+  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt", "@nuxtjs/color-mode"],
   shadcn: {
     /**
      * Prefix for all the imported component
@@ -13,5 +13,13 @@ export default defineNuxtConfig({
      * @default "./components/ui"
      */
     componentDir: "./components/ui",
+  },
+  colorMode: {
+    preference: "system",
+    globalName: "__NUXT_COLOR_MODE__",
+    componentName: "ColorScheme",
+    classPrefix: "",
+    classSuffix: "",
+    storageKey: "nuxt-color-mode",
   },
 });
