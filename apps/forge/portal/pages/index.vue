77de1c4a8d40868b6a7d5da6a667defaeeb791fe
hey-ewan <ewanretorokugbe@gmail.com>
2025-02-07 14:02:35 +0100

    chore: set up main page

--- apps/forge/portal/pages/index.vue
+++ apps/forge/portal/pages/index.vue
@@ -28,4 +28,12 @@ import { Button } from "~/components/ui/button";
       </Breadcrumb>
     </div>
   </header>
+  <main class="flex w-full h-full flex-col justify-center items-center">
+    <h1 class="text-2xl font-bold text-center">Welcome to The Forge Portal</h1>
+    <p class="my-4 text-center">Where do you want to start?</p>
+    <div class="flex flex-col md:flex-row gap-3 mt-2">
+      <Button>Start a new project</Button>
+      <Button variant="secondary">Set up a provider</Button>
+    </div>
+  </main>
 </template>
