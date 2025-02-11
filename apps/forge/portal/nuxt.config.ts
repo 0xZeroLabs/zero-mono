5d7461f10116d7961dff10e5869f14d9ab279c15
hey-ewan <ewanretorokugbe@gmail.com>
2025-02-11 19:31:42 +0100

    fix: node polyfills

--- apps/forge/portal/nuxt.config.ts
+++ apps/forge/portal/nuxt.config.ts
@@ -1,3 +1,4 @@
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
@@ -25,7 +26,54 @@ export default defineNuxtConfig({
   },
   vite: {
     define: {
-      global: "window",
+      global: "globalThis",
+    },
+
+    plugins: [
+      {
+        // work around for `exports is not defined` error within the crypto-browserify > randomfill dep
+        name: "crypto-randomfill-patch",
+
+        apply: "build",
+        enforce: "pre",
+
+        renderChunk(code, chunk) {
+          if (!/randomfill.*\.js$/.test(chunk.fileName)) {
+            return null;
+          }
+
+          console.log("🟪 Patching crypto randomfill dep");
+
+          const modifiedCode = code
+            .split("\n")
+            .map((line) => {
+              if (line.trim() === "exports.randomFill = randomFill;") {
+                return "var exports = {}; exports.randomFill = randomFill;";
+              }
+
+              return line;
+            })
+            .join("\n");
+
+          return { code: modifiedCode, map: null };
+        },
+      },
+      nodePolyfills({
+        include: ["crypto", "stream", "process", "path"],
+      }),
+    ],
+
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: (id) => {
+            // isolate randomfill as a separate chunk
+            if (id.includes("node_modules/randomfill/browser.js")) {
+              return "randomfill";
+            }
+          },
+        },
+      },
     },
   },
 });
