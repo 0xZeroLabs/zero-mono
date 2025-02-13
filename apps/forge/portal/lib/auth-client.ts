78d0c803db1b97c97e4dec50fcfb680dbc1ec2c2
hey-ewan <ewanretorokugbe@gmail.com>
2025-02-13 04:48:49 +0100

    refactor: use `BASE_URL` to differenciate production

--- apps/forge/portal/lib/auth-client.ts
+++ apps/forge/portal/lib/auth-client.ts
@@ -2,6 +2,6 @@ import { createAuthClient } from "better-auth/vue";
 import { magicLinkClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000",
+  baseURL: process.env.BASE_URL,
   plugins: [magicLinkClient()],
 });
