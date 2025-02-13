2d36ba8c62cbb5cb0c61793deff2077a5f468fec
hey-ewan <ewanretorokugbe@gmail.com>
2025-02-13 04:22:05 +0100

    refactor: removed polyfills build

--- apps/forge/portal/nuxt.config.ts
+++ apps/forge/portal/nuxt.config.ts
@@ -1,4 +1,4 @@
-import { nodePolyfills } from "vite-plugin-node-polyfills";
+// import { nodePolyfills } from "vite-plugin-node-polyfills";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
@@ -30,56 +30,50 @@ export default defineNuxtConfig({
     },
 
     plugins: [
-      {
-        // work around for `exports is not defined` error within the crypto-browserify > randomfill dep
-        name: "crypto-randomfill-patch",
-
-        apply: "build",
-        enforce: "pre",
-
-        renderChunk(code, chunk) {
-          if (!/randomfill.*\.js$/.test(chunk.fileName)) {
-            return null;
-          }
-
-          console.log("🟪 Patching crypto randomfill dep");
-
-          const modifiedCode = code
-            .split("\n")
-            .map((line) => {
-              if (line.trim() === "exports.randomFill = randomFill;") {
-                return "var exports = {}; exports.randomFill = randomFill;";
-              }
-
-              return line;
-            })
-            .join("\n");
-
-          return { code: modifiedCode, map: null };
-        },
-      },
-      nodePolyfills({
-        include: ["crypto", "stream", "process", "path", "string_decoder"],
-      }),
+      // {
+      //   // work around for `exports is not defined` error within the crypto-browserify > randomfill dep
+      //   name: "crypto-randomfill-patch",
+      //   apply: "build",
+      //   enforce: "pre",
+      //   renderChunk(code, chunk) {
+      //     if (!/randomfill.*\.js$/.test(chunk.fileName)) {
+      //       return null;
+      //     }
+      //     console.log("🟪 Patching crypto randomfill dep");
+      //     const modifiedCode = code
+      //       .split("\n")
+      //       .map((line) => {
+      //         if (line.trim() === "exports.randomFill = randomFill;") {
+      //           return "var exports = {}; exports.randomFill = randomFill;";
+      //         }
+      //         return line;
+      //       })
+      //       .join("\n");
+      //     return { code: modifiedCode, map: null };
+      //   },
+      // },
+      // nodePolyfills({
+      //   include: ["crypto", "stream", "process", "path", "string_decoder"],
+      // }),
     ],
 
-    build: {
-      rollupOptions: {
-        output: {
-          manualChunks: (id) => {
-            // isolate randomfill as a separate chunk
-            if (id.includes("node_modules/randomfill/browser.js")) {
-              return "randomfill";
-            }
-          },
-        },
-      },
-    },
+    // build: {
+    //   rollupOptions: {
+    //     output: {
+    //       manualChunks: (id) => {
+    //         // isolate randomfill as a separate chunk
+    //         if (id.includes("node_modules/randomfill/browser.js")) {
+    //           return "randomfill";
+    //         }
+    //       },
+    //     },
+    //   },
+    // },
   },
 
-  nitro: {
-    externals: {
-      inline: ["crypto", "stream", "process", "path", "string_decoder"],
-    },
-  },
+  // nitro: {
+  //   externals: {
+  //     inline: ["crypto", "stream", "process", "path", "string_decoder"],
+  //   },
+  // },
 });
